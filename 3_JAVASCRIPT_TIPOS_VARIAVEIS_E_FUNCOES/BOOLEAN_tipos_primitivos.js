da31be55e8ef9f8693d7ace5adcf0d09e586ed74
Marlon <marlonprado04@gmail.com>
2023-06-27 12:30:41 -0300

    add aula de tipo primitivo BOOLEAN

---
+++ 3_JAVASCRIPT_TIPOS_VARIAVEIS_E_FUNCOES/BOOLEAN_tipos_primitivos.js
@@ -0,0 +1,22 @@
+
+// Criando variaveis e constante
+const n1 = 5;
+const n2 = 10;
+
+// Comparando valores das constantes
+
+console.log(n1 === n2); // retorna false
+
+// Criando uma 3 constante com valor igual à 1
+const n3 = 5;
+
+console.log(n1 === n3); // retorna true
+
+// Criando constantes de string
+
+const texto1 = "Alura";
+const texto2 = "a";
+
+console.log(texto1 === texto2); // retorna false
+
+// Criando outra constante de string
