e81ef57b92c0c776ff0e56e0acfe46ac602783e3
Marlon <marlonprado04@gmail.com>
2023-07-18 23:57:23 -0300

    atualizei comentários do main.js

--- 10_JAVASCRIPT_ARMAZENANDO_DADOS_NO_NAVEGADOR/js/main.js
+++ 10_JAVASCRIPT_ARMAZENANDO_DADOS_NO_NAVEGADOR/js/main.js
@@ -1,49 +1,47 @@
-// Criando variável para armazenar o acesso ao formulário
+// Criando variáveis de acesso ao DOM
 const form = document.getElementById("novoItem");
-
-// Criando variável para armazenar o acesso à lista de itens
 const lista = document.getElementById("lista");
 
-// Criando escutador de evento para ao clicar no "submit" printar no console "funcionou" 
+// Criando listener para o formulário
 form.addEventListener("submit", (evento) => {
-    // Criando comando para previnir ação padrão do evento, que nesse caso seria o submit para a própria página, assim o console.log vai funcionar ao clicar no submit
+    // Impedindo reload da página submitar
     evento.preventDefault();
 
-    // PCriando variável para conter nome e quantidade preenchidos no formulário do site
+    // Criando variáveis para acessar valores dos inputs
     const nome = evento.target.elements["nome"].value;
     const quantidade = evento.target.elements["quantidade"].value;
 
-    // Chamando função criaElemento toda vez que o botão submit for clicado e passando nome e quantidade
+    // Chamando função para criar item na lista
     criaElemento(nome, quantidade);
 
 })
 
-// Criando função que recebe nome e quantidade para criar um novo elemento
+// Criando para criar novo elemento e incluir na lista
 function criaElemento(nome, quantidade) {
     console.log(nome); // para testar se está imprimindo valor do nome
     console.log(quantidade); // para testar se está imprimindo valor da quantidade
 
     // Exemplo de tag item: <li class="item"><strong>7</strong>Camisas</li>
 
-    // Criando variável para conter um novo "li" criado no html 
+    // Declarando variável para criar "li"
     const novoItem = document.createElement("li");
-    // Atribuindo ao novo item criado a classe "item"
+    // Atribuindo à variável "li" a classe "item"
     novoItem.classList.add("item");
 
-    // Criando variável para conter um novo elemento "strong" criado no html
+    // Declarando variável para criar o "strong"
     const numeroItem = document.createElement("strong");
-    // Passando a quantidade informada no formulário para o valor dentro da tag html criada na variavel numeroItem (innerHTML -> passa valor para dentro da tag)
+    // Passando quantidade informada no formulário para variável do "strong"
     numeroItem.innerHTML = quantidade;
 
-    // Passando a variável numeroItem para dentro da novoItem via metodo appendChield (para comportar toda tag HTML, sem que seja adicionada como um objeto)
+    // Adicionando à variável "li" a tag "strong"
     novoItem.appendChild(numeroItem);
-    // Agora adicionando a variável nome à variável novoItem via innerHTML, pois ela já está com a quantidade dentro da tag
+    // Adicionando à variável "li" o valor do nome
     novoItem.innerHTML += nome;
 
-    // Adicionando a variável novoItem à variável que contem toda a tag com a lista de itens
+    // Adicionando a tag "li" à tag "ul" (variável lista)
     lista.appendChild(novoItem);
 
-    // Printando variável novoItem para confirmar se o innerHTML funcionou
+    // Printando variável novoItem para confirmar se funcionou
     console.log(novoItem);
 }
 
