3fdb28435b71cdf298a9a1a9b7a4ee54bdde2ae4
Marlon <marlonprado04@gmail.com>
2023-07-20 17:00:25 -0300

    ajustei código de double no java

--- 11_JAVA_JRE_E_JDK/02_aula_eclipse/src/sintaxe_variaveis_e_fluxo/TestaPontoFlutuante.java
+++ 11_JAVA_JRE_E_JDK/02_aula_eclipse/src/sintaxe_variaveis_e_fluxo/TestaPontoFlutuante.java
@@ -8,25 +8,32 @@ public class TestaPontoFlutuante {
 
 	// Printando variável com texto
 	System.out.println("Meu salário é: " + salario);
-	
+
+	// O código abaixo funcionaria, pois a variável idade foi criada mais abaixo
+	// System.out.println("Minha idade é: " + idade);
+
 	// Adicionando um número inteiro à variável double
 	double idade = 23;
 	System.out.println("Minha idade é: " + idade);
-	
+
 	// Ao tentar adicionar 0.0 em variável int ele não aceita
 	// int valor = 0.0;
-	
+
 	// Fazendo contas com double
 	double divisao = 3.14 / 2;
 	System.out.println("O resultado é: " + divisao);
-	
+
 	// Dividindo 2 inteiros para demonstrar resultado
-	int outraDivisao = 5 / 2; // Retorna 2, um inteiro arredondado
+	int outraDivisao = 5 / 2; // Retorna 2
 	System.out.println("O resultado é: " + outraDivisao);
 
 	// Atribuindo mesma divisão para 2 inteiros
-	double novaTentativa = 5 / 2; // Retorna 2.0, pois ele trabalha da esquerda pra direita
+	double novaTentativa = 5 / 2; // Retorna 2.0
 	System.out.println("O novo resultado: " + novaTentativa);
+
+	// Atribuindo mesma divisão, agora 1 sendo double
+	double maisUmaTentativa = 5.0 / 2; // Retorna 2.5
+	System.out.println("O novo resultado: " + maisUmaTentativa);
     }
 
 }
