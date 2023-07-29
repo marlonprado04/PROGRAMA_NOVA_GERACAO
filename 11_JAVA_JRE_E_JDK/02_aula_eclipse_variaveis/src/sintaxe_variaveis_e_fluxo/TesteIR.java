4dfa78bd9832499bc133e009a9bc57cf3309d239
Marlon <marlonprado04@gmail.com>
2023-07-29 18:54:20 -0300

    ajustei identação do exercício de calculo de ir

--- 11_JAVA_JRE_E_JDK/02_aula_eclipse_variaveis/src/sintaxe_variaveis_e_fluxo/TesteIR.java
+++ 11_JAVA_JRE_E_JDK/02_aula_eclipse_variaveis/src/sintaxe_variaveis_e_fluxo/TesteIR.java
@@ -8,24 +8,17 @@ public class TesteIR {
 		// Criando lógica de comparação
 		if (salario < 1900.0) {
 			System.out.println("Seu salário é menor do que o esperado!");
+		} else if (salario >= 1900.0 && salario <= 2800.0) {
+			System.out.println("O IR é de 7,5%");
+			System.out.println("Você pode deduzir R$142.");
+		} else if (salario >= 2800.01 && salario <= 3751.0) {
+			System.out.println("O IR é de 15%");
+			System.out.println("Você pode deduzir R$350");
+		} else if (salario >= 3751.01 && salario <= 4664.00) {
+			System.out.println("O IR é de 22.5%");
+			System.out.println("Você pode deduzir R$636");
 		} else {
-			if (salario >= 1900.0 && salario <= 2800.0) {
-				System.out.println("O IR é de 7,5%");
-				System.out.println("Você pode deduzir R$142.");
-			} else {
-				if (salario >= 2800.01 && salario <= 3751.0) {
-					System.out.println("O IR é de 15%");
-					System.out.println("Você pode deduzir R$350");
-				} else {
-					if (salario >= 3751.01 && salario <= 4664.00) {
-						System.out.println("O IR é de 22.5%");
-						System.out.println("Você pode deduzir R$636");
-					} else {
-						System.out.println("Seu salário é maior do que o esperado!");
-					}
-				}
-			}
+			System.out.println("Seu salário é maior do que o esperado!");
 		}
-
 	}
 }
